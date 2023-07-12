8b197dea873eebce4032aa1107e4e6b2f144390b
Ivan <v.chester@yandex.ru>
2023-07-12 23:56:37 +0300

    Прошел постман

---
+++ src/main/java/ru/practicum/shareit/exception/handler/ErrorHandler.java
@@ -0,0 +1,37 @@
+package ru.practicum.shareit.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.exception.ErrorResponse;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.ValidationException;
+import ru.practicum.shareit.exception.WrongAccessException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        return new ErrorResponse(
+                e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleEmailExistException(final WrongAccessException e) {
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+}
