739960384ad1ba46f8ab6f171b6d69bbecdf8fae
Ivan <v.chester@yandex.ru>
2023-08-21 04:07:11 +0300

    test

---
+++ server/src/main/java/ru/practicum/shareit/create/Constant.java
@@ -0,0 +1,6 @@
+package ru.practicum.shareit.create;
+
+public class Constant {
+
+    public static final String HEADER_USER = "X-Sharer-User-Id";
+}
---
+++ server/src/test/java/ru/practicum/shareit/create/UnionServiceTest.java
@@ -0,0 +1,58 @@
+package ru.practicum.shareit.create;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.practicum.shareit.booking.BookingStorage;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.item.ItemStorage;
+import ru.practicum.shareit.request.ItemRequestStorage;
+import ru.practicum.shareit.user.UserStorage;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UnionServiceTest {
+
+    @Autowired
+    private UnionService unionService;
+
+    @MockBean
+    private UserStorage userStorage;
+
+    @MockBean
+    private ItemStorage itemStorage;
+
+    @MockBean
+    private BookingStorage bookingStorage;
+
+    @MockBean
+    private ItemRequestStorage itemRequestStorage;
+
+    @Test
+    void checkUser() {
+        when(userStorage.existsById(anyLong())).thenReturn(false);
+        assertThrows(NotFoundException.class, () -> unionService.checkUser(1L));
+    }
+
+    @Test
+    void checkItem() {
+        when(itemStorage.existsById(anyLong())).thenReturn(false);
+        assertThrows(NotFoundException.class, () -> unionService.checkItem(1L));
+    }
+
+    @Test
+    void checkBooking() {
+        when(bookingStorage.existsById(anyLong())).thenReturn(false);
+        assertThrows(NotFoundException.class, () -> unionService.checkBooking(1L));
+    }
+
+    @Test
+    void checkRequest() {
+        when(itemRequestStorage.existsById(anyLong())).thenReturn(false);
+        assertThrows(NotFoundException.class, () -> unionService.checkRequest(1L));
+    }
+}
